3512d1ae23576210463b62f5c08006dc736ab282
vasvi-sood <contactvasvisood@gmail.com>
2021-07-09 13:33:20 +0530

    added sigterm handling

--- examples/ding-dong-bot.js
+++ examples/ding-dong-bot.js
@@ -2,6 +2,28 @@
  * Wechaty - Conversational RPA SDK for Chatbot Makers.
  *  - https://github.com/wechaty/wechaty
  */
+ const http = require('http');
+
+ process
+   .on('SIGTERM', shutdown('SIGTERM'))
+   .on('SIGINT', shutdown('SIGINT'))
+   .on('uncaughtException', shutdown('uncaughtException'));
+
+ setInterval(console.log.bind(console, 'tick'), 1000);
+ http.createServer((req, res) => res.end('hi'))
+   .listen(process.env.PORT || 3000, () => console.log('Listening'));
+
+ function shutdown(signal) {
+   return (err) => {
+     console.log(`${ signal }...`);
+     if (err) console.error(err.stack || err);
+     setTimeout(() => {
+       console.log('...waited 5s, exiting.');
+       process.exit(err ? 1 : 0);
+     }, 5000).unref();
+   };
+ }
+
 const {
   Wechaty,
   ScanStatus,
